66ade5879844ea3fd60874428fb5263380139ad9
aslskks <130728002+aslskks@users.noreply.github.com>
2023-09-28 15:06:51 -0600

    code

--- app.py
+++ app.py
@@ -37,4 +37,5 @@ def serve_static(filename):
     return send_from_directory('static', filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
