d191967c1890ea4fa23113082c49092ed04538e4
aslskks <davikenat@gmail.com>
2023-09-29 11:04:22 -0600

    code

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, send_from_directory
-from werkzeug.utils import secure_filename
 import qrcode
+from PIL import Image
+from flask import Flask, render_template, request, send_from_directory, Response
 import os
+from io import BytesIO
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'static/qrcodes'
@@ -13,24 +14,44 @@ def index():
 
 @app.route('/generate_qr', methods=['POST'])
 def generate_qr():
-    url = request.form['url']
-    filename = secure_filename(request.form['filename'])
-    
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-    )
-    qr.add_data(url)
-    qr.make(fit=True)
-    
-    img = qr.make_image(fill_color="black", back_color="white")
-    img_filename = f'{filename}_qrcode.png'
-    img_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/qrcodes', 'qrcode.png')
-    img.save(img_path)
-    
-    return render_template('qrcode.html', img_filename=img_filename)
+    data_type = request.form['data_type']
+    data = request.form['data']
+    filename = request.form['filename']
+
+    if data_type == 'url':
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(data)
+        qr.make(fit=True)
+
+        img = qr.make_image(fill_color="black", back_color="white")
+        img_filename = f'{filename}_qrcode.png'  # Nombre de archivo basado en el nombre proporcionado por el usuario
+        img_path = os.path.join(app.config['UPLOAD_FOLDER'], img_filename)
+        img.save(img_path)
+    else:
+        # Para datos de texto
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(data)
+        qr.make(fit=True)
+
+        img = qr.make_image(fill_color="black", back_color="white")
+        img_filename = f'{filename}_text_qrcode.png'  # Nombre de archivo basado en el nombre proporcionado por el usuario
+        img_path = os.path.join(app.config['UPLOAD_FOLDER'], img_filename)
+        img.save(img_path)
+
+    # Devuelve la imagen como una respuesta HTTP
+    with open(img_path, 'rb') as img_file:
+        img_data = img_file.read()
+    return Response(img_data, content_type='image/png')
 
 @app.route('/static/<filename>')
 def serve_static(filename):
@@ -38,4 +59,3 @@ def serve_static(filename):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
